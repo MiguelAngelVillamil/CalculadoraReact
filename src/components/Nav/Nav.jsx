4de6e5ae3b4400652de48937383ecc0b52d9d02f
Miguel Angel Villamil <angelvillamil1234@gmail.com>
2022-10-13 20:55:47 -0300

    Router funcional con manejo de clases

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export default function Nav() {
 
     return(
         <nav className='nav'>
-            <button>Angel</button>
-            <button>Victor</button>
+            <Link to="/angel">
+                <button>Angel</button>
+            </Link>
+            <Link to="/victor">
+              <button>Victor</button>
+            </Link>
         </nav>
     );
 }
